7111f1f7833af14a03aef1037b515e828bce0bd2
Steven <steven.leego@gmail.com>
2018-02-25 17:25:10 +0200

    Esialgne rakendus töötab

--- src/main/java/ee/ut/veebirakendus/epood/EpoodApplication.java
+++ src/main/java/ee/ut/veebirakendus/epood/EpoodApplication.java
@@ -1,12 +1,21 @@
 package ee.ut.veebirakendus.epood;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
+@RestController
+@EnableAutoConfiguration
 public class EpoodApplication {
 
-	public static void main(String[] args) {
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EpoodApplication.class, args);
 	}
 }
