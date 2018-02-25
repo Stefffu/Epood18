c2761c9445cf46cc092061c75e60dbde06a37424
Steven <steven.leego@gmail.com>
2018-02-25 16:30:25 +0200

    Initial commit

---
+++ src/main/java/ee/ut/veebirakendus/epood/EpoodApplication.java
@@ -0,0 +1,12 @@
+package ee.ut.veebirakendus.epood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpoodApplication.class, args);
+	}
+}
