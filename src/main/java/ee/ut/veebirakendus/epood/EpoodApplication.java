7359926703c225f0db53890264f8b8cd393c1311
Steven <steven.leego@gmail.com>
2018-03-18 15:13:09 +0200

    Lihtne rakendus sisselogimisega + html templiidid

--- src/main/java/ee/ut/veebirakendus/epood/EpoodApplication.java
+++ src/main/java/ee/ut/veebirakendus/epood/EpoodApplication.java
@@ -6,14 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@EnableAutoConfiguration
+@SpringBootApplication
 public class EpoodApplication {
 
-	@RequestMapping("/")
+	/*@RequestMapping("/")
 	String home() {
 		return "Hello World!!!";
-	}
+	}*/
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(EpoodApplication.class, args);
